ab1a2820e24e6f044588615917eb956fec8db253
MRiabov <maksymriabov2004@gmail.com>
2022-08-18 17:38:32 +0100

    DB mapping in progress...

--- src/main/java/edu/mriabov/challengertelegrambot/repository/enums/Area.java
+++ src/main/java/edu/mriabov/challengertelegrambot/repository/enums/Area.java
@@ -1,10 +1,18 @@
 package edu.mriabov.challengertelegrambot.repository.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Area {
 
-    RELATIONSHIPS,
-    FITNESS,
-    MINDFULNESS,
-    FINANCES
+    RELATIONSHIPS("RELATIONSHIPS"),
+    FITNESS("FITNESS"),
+    MINDFULNESS("MINDFULNESS"),
+    FINANCES("FINANCES");
 
+    private final String text;
+
+    Area(String text) {
+        this.text = text;
+    }
 }
--- src/main/java/edu/mriabov/challengertelegrambot/repository/model/Chat.java
+++ src/main/java/edu/mriabov/challengertelegrambot/repository/model/Chat.java
@@ -11,7 +11,6 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
-
 public class Chat {
 
     @Id
