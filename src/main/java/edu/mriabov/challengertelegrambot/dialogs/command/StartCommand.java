f34fbc1041b3cfbc8fdc7e55e13d661f3f304d4b
MRiabov <maksymriabov2004@gmail.com>
2022-08-22 20:58:26 +0100

    ITS WORKIINNG!!!

--- src/main/java/edu/mriabov/challengertelegrambot/dialogs/command/StartCommand.java
+++ src/main/java/edu/mriabov/challengertelegrambot/dialogs/command/StartCommand.java
@@ -7,30 +7,39 @@ import edu.mriabov.challengertelegrambot.utils.ButtonsUtils;
 import edu.mriabov.challengertelegrambot.utils.ReplyUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.abilitybots.api.objects.Locality;
+import org.telegram.abilitybots.api.objects.Privacy;
 import org.telegram.abilitybots.api.objects.ReplyFlow;
 import org.telegram.abilitybots.api.util.AbilityExtension;
 
 @RequiredArgsConstructor
 @Slf4j
+@Service
 public class StartCommand implements AbilityExtension {
 
     private final TelegramBot telegramBot;
 
-    public Ability onStart(){
+    public Ability onStart() {
         return Ability.builder()
                 .name("start")
                 .info("(re)Starts the bot.")
-                .reply(ReplyFlow.builder(telegramBot.db())
-                        .action((baseAbilityBot, update) -> ButtonsUtils.buildSendMessageWithKeyboard(
-                                update.getMessage().getChatId(),Buttons.ON_START_NEW_USER)
+                .privacy(Privacy.PUBLIC)
+                .locality(Locality.USER)
+                .input(0)
+                .action(messageContext -> telegramBot.silent().execute(
+                        ButtonsUtils.buildSendMessageWithKeyboard(
+                        messageContext.chatId(), Buttons.ON_START_NEW_USER)))
 
-                        ).next(ReplyUtils.buildFlow(ReceivedMessages.ON_START_NEW_USER_YES,telegramBot))
-                        .next(ReplyUtils.buildFlow(ReceivedMessages.ON_START_NEW_USER_NO,telegramBot))
+                .reply(ReplyFlow.builder(telegramBot.db())
+
+                        .next(ReplyUtils.buildFlow(ReceivedMessages.ON_START_NEW_USER_YES, telegramBot))
+                        .next(ReplyUtils.buildFlow(ReceivedMessages.ON_START_NEW_USER_NO, telegramBot))
                         .build())
 
 
-        .build();
+                .build();
     }
 
 }
--- src/main/java/edu/mriabov/challengertelegrambot/service/TelegramBot.java
+++ src/main/java/edu/mriabov/challengertelegrambot/service/TelegramBot.java
@@ -1,6 +1,7 @@
 package edu.mriabov.challengertelegrambot.service;
 
 import edu.mriabov.challengertelegrambot.config.BotConfig;
+import edu.mriabov.challengertelegrambot.dialogs.command.StartCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.abilitybots.api.bot.AbilityBot;
@@ -11,10 +12,10 @@ public class TelegramBot extends AbilityBot {
     final BotConfig config;
 
     @Autowired
-    public TelegramBot(BotConfig config) {
+    public TelegramBot(BotConfig config/*, @Autowired List<AbilityExtension> list*/) {
         super(config.getToken(), config.getBotName());
         this.config = config;
-
+        addExtensions(new StartCommand(this));
     }
 
     @Override
