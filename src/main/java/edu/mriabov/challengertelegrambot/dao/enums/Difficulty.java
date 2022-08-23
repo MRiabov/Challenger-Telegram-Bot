843ee35d6e6ca677411a477924a2ab4135b31f35
MRiabov <maksymriabov2004@gmail.com>
2022-08-23 23:40:36 +0100

    thx to my CodeReviewer

--- src/main/java/edu/mriabov/challengertelegrambot/dao/enums/Difficulty.java
+++ src/main/java/edu/mriabov/challengertelegrambot/dao/enums/Difficulty.java
@@ -2,14 +2,8 @@ package edu.mriabov.challengertelegrambot.dao.enums;
 
 public enum Difficulty {
 
-    EASY("EASY"),
-    MEDIUM("MEDIUM"),
-    DIFFICULT("DIFFICULT"),
-    GOAL("GOAL");
-
-    private final String text;
-
-    Difficulty(String text) {
-        this.text=text;
-    }
+    EASY,
+    MEDIUM,
+    DIFFICULT,
+    GOAL
 }
