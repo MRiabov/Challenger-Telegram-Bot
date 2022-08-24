08bbcbe8a1f8de672c6fe3fadc88c530298035db
MRiabov <maksymriabov2004@gmail.com>
2022-08-24 23:43:48 +0100

    still making frontend... so much to go...

--- src/main/java/edu/mriabov/challengertelegrambot/service/ReplyBuilderService.java
+++ src/main/java/edu/mriabov/challengertelegrambot/service/ReplyBuilderService.java
@@ -4,13 +4,14 @@ import edu.mriabov.challengertelegrambot.dialogs.buttons.ReceivedMessages;
 import org.springframework.stereotype.Service;
 import org.telegram.abilitybots.api.objects.ReplyFlow;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Service
 public interface ReplyBuilderService {
 
-   ReplyFlow buildSimpleFlow(int id,@NotNull ReceivedMessages receivedMessages, @NotNull List<ReplyFlow> nextFlow);
-   ReplyFlow buildSimpleFlow(int id,@NotNull ReceivedMessages receivedMessages, @NotNull ReplyFlow nextFlow);
+   ReplyFlow buildSimpleFlow(int id,@NotNull ReceivedMessages receivedMessages, @NotNull @NotEmpty List<ReplyFlow> nextFlow);
+   ReplyFlow buildSimpleFlow(int id,@NotNull ReceivedMessages receivedMessages, @NotNull @NotEmpty ReplyFlow nextFlow);
 
 }
