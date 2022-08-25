3be14c2419c6023de6a3754b0fbc3ca381df62a5
MRiabov <maksymriabov2004@gmail.com>
2022-08-25 22:24:36 +0100

    damn, the bot is launchable after 3 days, wow. and it doesn't work. damn.

--- src/main/java/edu/mriabov/challengertelegrambot/service/ReplyBuilderService.java
+++ src/main/java/edu/mriabov/challengertelegrambot/service/ReplyBuilderService.java
@@ -1,7 +1,9 @@
 package edu.mriabov.challengertelegrambot.service;
 
+import edu.mriabov.challengertelegrambot.dialogs.buttons.Buttons;
 import edu.mriabov.challengertelegrambot.dialogs.buttons.ReceivedMessages;
 import org.springframework.stereotype.Service;
+import org.telegram.abilitybots.api.objects.Reply;
 import org.telegram.abilitybots.api.objects.ReplyFlow;
 
 import javax.validation.constraints.NotEmpty;
@@ -11,7 +13,11 @@ import java.util.List;
 @Service
 public interface ReplyBuilderService {
 
-   ReplyFlow buildSimpleFlow(int id,@NotNull ReceivedMessages receivedMessages, @NotNull @NotEmpty List<ReplyFlow> nextFlow);
-   ReplyFlow buildSimpleFlow(int id,@NotNull ReceivedMessages receivedMessages, @NotNull @NotEmpty ReplyFlow nextFlow);
+   ReplyFlow buildSimpleFlow(int id,@NotNull ReceivedMessages receivedMessages, @NotNull @NotEmpty List<Reply> nextFlow);
+   ReplyFlow buildSimpleFlow(int id,@NotNull ReceivedMessages receivedMessages, @NotNull @NotEmpty Reply nextFlow);
+   Reply buildSimpleReply(@NotNull ReceivedMessages receivedMessages);
+
+   Reply cancelMessage(@NotNull Buttons buttons);
+
 
 }
