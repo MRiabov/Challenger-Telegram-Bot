b22abe8077531bebfd7d42150052c0daadb7cea5
MRiabov <maksymriabov2004@gmail.com>
2022-09-18 00:18:39 +0200

    deleted useless command handling

--- src/main/java/edu/mriabov/challengertelegrambot/handler/impl/GroupMasterMessageHandler.java
+++ src/main/java/edu/mriabov/challengertelegrambot/handler/impl/GroupMasterMessageHandler.java
@@ -1,6 +1,5 @@
 package edu.mriabov.challengertelegrambot.handler.impl;
 
-import edu.mriabov.challengertelegrambot.commands.CommandContainer;
 import edu.mriabov.challengertelegrambot.handler.MessageHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,14 +10,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @RequiredArgsConstructor
 @Slf4j
 public class GroupMasterMessageHandler implements MessageHandler {
-    private final CommandContainer commandContainer;
 
     @Override
     public void handleMessages(Update update) {
-        if (update.getMessage().isCommand()) {
-            log.info("Received a command from chat " + update.getMessage().getChat().getTitle() +
-                    " with text " + update.getMessage().getText());
-//            commandContainer.executeByText(update.getMessage());
-        }
+
     }
 }
